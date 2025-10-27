f685d897fefbd1234818b127e586874eb6009428
Ewa Szalwinska <e.szalwinska@gmail.com>
2025-10-27 15:04:29 -0700

    creates a list of guest attending event

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const COHORT = "/2509-Pt-EwaSz."; // Make sure to change this!
 const RESOURCE = "/events";
 const API = BASE + COHORT + RESOURCE;
 
+const GUESTS = "/guests";
+const GUESTSAPI = BASE + COHORT + GUESTS;
+
+const RSVPS = "/rsvps";
+const RSVPSAPI = BASE + COHORT + RSVPS;
+
+// The user can also see the guests who have RSVP'd to the selected party.
+// This will involve fetching data from the /rsvps and /guests endpoints!
+// Fetch all the guests first, and then use the RSVPs to filter the guests for the selected party.
+
 let events = [];
+let guests = [];
+let rsvps = [];
 let selectedEvent;
+let eventRsvps;
 
 async function getEvents() {
   try {
@@ -28,6 +41,40 @@ async function getEvent(id) {
   }
 }
 
+// async function getOneRSVP(eventId) {
+//   try {
+//     let response = await fetch(API + "/" + eventId);
+//     let result = await response.json();
+//     console.log("getOneRSVP", getOneRSVP);
+//     let oneRSVP = result.data;
+//     console.log("oneRSVP", oneRSVP);
+//   } catch (error) {
+//     console.error("something went wrong", error);
+//   }
+// }
+
+async function getRSVP() {
+  try {
+    let response = await fetch(RSVPSAPI);
+    let result = await response.json();
+    rsvps = result.data;
+    console.log("rsvps", rsvps);
+  } catch (error) {
+    console.error("something went wrong", error);
+  }
+}
+
+async function getGuests() {
+  try {
+    let response = await fetch(GUESTSAPI);
+    let result = await response.json();
+    guests = result.data;
+    console.log("guests", guests);
+  } catch (error) {
+    console.error("something went wrong", error);
+  }
+}
+
 function EventListItem(thing) {
   let $li = document.createElement("li");
   let $a = document.createElement("a");
@@ -37,6 +84,12 @@ function EventListItem(thing) {
     e.preventDefault();
     console.log("thing", thing);
     await getEvent(thing.id);
+    await getRSVP();
+    await getGuests();
+    eventRsvps = rsvps.filter((rsvp) => {
+      if (rsvp.eventId === thing.id) return rsvp;
+    });
+    console.log("eventRsvps", eventRsvps);
     render();
   });
   $li.appendChild($a);
@@ -48,6 +101,28 @@ function EventList() {
   $ul.classList.add("eventList");
   const $events = events.map(EventListItem);
   $ul.replaceChildren(...$events);
+  return $ul;
+}
+
+function GuestItem(guest) {
+  let $li = document.createElement("li");
+  $li.textContent = guest.name;
+  return $li;
+}
+
+function EventGuestList() {
+  let attendingGuests = [];
+  for (let guest of guests) {
+    for (let rsvp of eventRsvps) {
+      if (guest.id === rsvp.guestId) {
+        attendingGuests.push(guest);
+      }
+    }
+  }
+  const $ul = document.createElement("ul");
+  $ul.classList.add("guestList");
+  const $guests = attendingGuests.map(GuestItem);
+  $ul.replaceChildren(...$guests);
   return $ul;
 }
 
@@ -64,7 +139,11 @@ function EventDetail() {
       <p>Location: ${selectedEvent.location}</p>
       <p>When: ${selectedEvent.date}</p>
       <p>${selectedEvent.description}</p>
+      <h4>Guests Attending:</h4>
       `;
+    const $guestList = EventGuestList();
+    $event.appendChild($guestList);
+
     return $event;
   }
 }
